15a059d7063c31c62093f4d9b1cd09be962fff85
Phineas Kibbey <phineas.kibbey@gmail.com>
2017-01-16 17:34:28 -0800

    Update links and output hash

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('hashchange', () => {
+  // Output current hash
+  console.log('complete hash = ', window.location.hash);
+
+  // strip the first hash and the forward slash from the string
+  let urlHash = window.location.hash.substring(2);
+
+  console.log('urlHash = ', urlHash);
+
+});
